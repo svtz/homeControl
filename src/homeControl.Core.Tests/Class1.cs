f260665f05d9c0ee887f255f3dd4e019c32a73ce
Svyatoslav Zakhryapin <svt.zhr@gmail.com>
2016-12-04 03:19:34 +0300

    setup xunit

--- src/homeControl.Core.Tests/Class1.cs
+++ src/homeControl.Core.Tests/Class1.cs
@@ -1,14 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Xunit;
 
 namespace homeControl.Core.Tests
 {
     public class Class1
     {
-        public Class1()
+        [Fact]
+        public void PassingTest()
         {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
         }
     }
 }
