59ae7a7e41ea7d9608e3ac58e0e893fe12b8f289
Svyatoslav Zakhryapin <svt.zhr@gmail.com>
2016-12-05 22:28:19 +0300

    Global AssemblyInfo

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("homeControl")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: AssemblyVersion("0.1.0")]
--- src/homeControl.Core/Properties/AssemblyInfo.cs
+++ src/homeControl.Core/Properties/AssemblyInfo.cs
@@ -1,14 +1,5 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("homeControl.Core")]
-[assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -18,4 +9,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("078591fc-60a6-45d0-854f-aa5f077d72c5")]
 
+// Internals visible to tests
 [assembly: InternalsVisibleTo("homeControl.Core.Tests")]
