23af23e9c3be6298588f562ce7eb597dd5b9d9e8
Svyatoslav Zakhryapin <svt.zhr@gmail.com>
2016-12-09 23:10:10 +0300

    experimental project

---
+++ src/homeControl.Experiments/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ThinkingHome.NooLite;
+
+namespace homeControl.Experiments
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var ad = new RX2164Adapter();
+            if (!ad.OpenDevice())
+                throw new Exception("unable to open");
+
+            ad.CommandReceived += Ad_CommandReceived;
+
+            while (true)
+            {
+                Thread.Sleep(100);
+            }
+
+            ad.Dispose();
+        }
+
+        private static void Ad_CommandReceived(ThinkingHome.NooLite.ReceivedData.ReceivedCommandData obj)
+        {
+            
+        }
+    }
+}
---
+++ src/homeControl.Experiments/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.InteropServices;
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("727f9156-ade4-4525-b07e-fe4a9680f768")]
