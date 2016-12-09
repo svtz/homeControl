144eecaa9a7750bf9349e9db67776ee91f0dc9b8
Svyatoslav Zakhryapin <svt.zhr@gmail.com>
2016-12-09 23:28:25 +0300

    base adapterWrapper

---
+++ src/homeControl.Noolite/Adapters/AbstractAdapterWrapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using homeControl.Peripherals;
+using ThinkingHome.NooLite;
+using ThinkingHome.NooLite.Common;
+
+namespace homeControl.Noolite
+{
+    internal class AdapterWrapperBase<TAdapter> : IDisposable where TAdapter : BaseAdapter, IPC11XXAdapter, IDisposable, new()
+    {
+        protected TAdapter Adapter { get; }
+
+        protected AdapterWrapperBase()
+        {
+            Adapter = new TAdapter();
+        }
+
+        protected string DeviceName => typeof(TAdapter).ToString();
+
+        protected void EnsureAdapterConnected()
+        {
+            if (!Adapter.IsConnected)
+                throw new DeviceDisconnectedException(DeviceName);
+
+            if (!Adapter.IsOpen)
+            {
+                if (!Adapter.OpenDevice())
+                    throw new DeviceInitializationException(DeviceName, "Could not open the device.");
+            }
+        }
+
+        public void Dispose()
+        {
+            Adapter.Dispose();
+        }
+    }
+}
---
+++ src/homeControl.Noolite/Adapters/SenderAdapterWrapper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using homeControl.Peripherals;
+using ThinkingHome.NooLite;
+using ThinkingHome.NooLite.Common;
+
+namespace homeControl.Noolite.Adapters
+{
+    internal sealed class SenderAdapterWrapper<TAdapter> : AdapterWrapperBase<TAdapter>, IPC11XXAdapter
+        where TAdapter : BaseAdapter, IPC11XXAdapter, IDisposable, new()
+    {
+        public void SendLedCommand(PC11XXLedCommand cmd, byte channel, byte levelR = 0, byte levelG = 0, byte levelB = 0)
+        {
+            EnsureAdapterConnected();
+
+            try
+            {
+                Adapter.SendLedCommand(cmd, channel, levelR, levelG, levelB);
+            }
+            catch (Exception ex)
+            {
+                throw new UnknownDeviceException(ex, DeviceName);
+            }
+        }
+
+        public void SendCommand(PC11XXCommand cmd, byte channel, byte level = 0)
+        {
+            EnsureAdapterConnected();
+
+            try
+            {
+                Adapter.SendCommand(cmd, channel, level);
+            }
+            catch (Exception ex)
+            {
+                throw new UnknownDeviceException(ex, DeviceName);
+            }
+        }
+    }
+}
