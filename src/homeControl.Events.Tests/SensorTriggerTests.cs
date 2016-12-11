80c67c3e03b5df6b6b6cc5e02527ceb205810e6a
Svyatoslav Zakhryapin <svt.zhr@gmail.com>
2016-12-11 03:27:52 +0300

    SensorTrigger

---
+++ src/homeControl.Events.Tests/SensorTriggerTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using homeControl.Core;
+using homeControl.Events.Sensors;
+using homeControl.Events.Switches;
+using homeControl.Events.Triggers;
+using Moq;
+using Xunit;
+
+namespace homeControl.Events.Tests
+{
+    public class SensorTriggerTests
+    {
+        public static IEnumerable<object[]> AbstractSensorEvents =>
+            new[]
+            {
+                new object[] { new SensorActivatedEvent(Guid.NewGuid()), },
+                new object[] { new SensorDeactivatedEvent(Guid.NewGuid()), },
+            };
+
+        [Theory]
+        [MemberData(nameof(AbstractSensorEvents))]
+        public void TestCanHandleSensorEvent(AbstractSensorEvent @event)
+        {
+            var handler = new SensorTrigger(Mock.Of<IEventPublisher>())
+            {
+                SwitchId = Guid.NewGuid(),
+                SensorId = @event.SensorId
+            };
+
+            Assert.True(handler.CanHandle(@event));
+        }
+
+        [Theory]
+        [MemberData(nameof(AbstractSensorEvents))]
+        public void DontProcessEventWithNonMatchedId(AbstractSensorEvent @event)
+        {
+            var handler = new SensorTrigger(Mock.Of<IEventPublisher>())
+            {
+                SwitchId = Guid.NewGuid(),
+                SensorId = Guid.NewGuid()
+            };
+
+            Assert.False(handler.CanHandle(@event));
+        }
+
+        [Fact]
+        public void TestCantHandleOtherEvents()
+        {
+            var handler = new SensorTrigger(Mock.Of<IEventPublisher>());
+            var @event = Mock.Of<IEvent>();
+
+            Assert.False(handler.CanHandle(@event));
+        }
+
+        [Fact]
+        public void TestTurnOn()
+        {
+            var sensorId = Guid.NewGuid();
+            var sensorActivatedEvent = new SensorActivatedEvent(sensorId);
+            var switchId = Guid.NewGuid();
+            var publisherMock = new Mock<IEventPublisher>(MockBehavior.Strict);
+            publisherMock.Setup(publisher => publisher.PublishEvent(It.Is<TurnOnEvent>(e => e.SwitchId == switchId)));
+
+            var handler = new SensorTrigger(publisherMock.Object)
+            {
+                SwitchId = switchId,
+                SensorId = sensorId
+            };
+            handler.Handle(sensorActivatedEvent);
+
+            publisherMock.Verify(sc => sc.PublishEvent(It.IsAny<IEvent>()), Times.Once);
+        }
+
+        [Fact]
+        public void TestTurnOff()
+        {
+            var sensorId = Guid.NewGuid();
+            var sensorActivatedEvent = new SensorDeactivatedEvent(sensorId);
+            var switchId = Guid.NewGuid();
+            var publisherMock = new Mock<IEventPublisher>(MockBehavior.Strict);
+            publisherMock.Setup(publisher => publisher.PublishEvent(It.Is<TurnOffEvent>(e => e.SwitchId == switchId)));
+
+            var handler = new SensorTrigger(publisherMock.Object)
+            {
+                SwitchId = switchId,
+                SensorId = sensorId
+            };
+            handler.Handle(sensorActivatedEvent);
+
+            publisherMock.Verify(sc => sc.PublishEvent(It.IsAny<IEvent>()), Times.Once);
+        }
+    }
+}
---
+++ src/homeControl.Events/Triggers/SensorTrigger.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using homeControl.Core;
+using homeControl.Events.Sensors;
+using homeControl.Events.Switches;
+
+namespace homeControl.Events.Triggers
+{
+    internal class SensorTrigger : IHandler
+    {
+        private readonly IEventPublisher _eventPublisher;
+
+        public SensorTrigger(IEventPublisher eventPublisher)
+        {
+            _eventPublisher = eventPublisher;
+        }
+
+        private Guid _switchId;
+        public Guid SwitchId
+        {
+            get { return _switchId; }
+            set
+            {
+                Guard.DebugAssertArgumentNotDefault(value, nameof(value));
+                _switchId = value;
+            }
+        }
+
+        private Guid _sensorId;
+        public Guid SensorId
+        {
+            get { return _sensorId; }
+            set
+            {
+                Guard.DebugAssertArgumentNotDefault(value, nameof(value));
+                _sensorId = value;
+            }
+        }
+
+        public bool CanHandle(IEvent @event)
+        {
+            Guard.DebugAssertArgumentNotNull(@event, nameof(@event));
+
+            var sensorEvent = @event as AbstractSensorEvent;
+            return sensorEvent != null && sensorEvent.SensorId == SensorId;
+        }
+
+        public void Handle(IEvent @event)
+        {
+            Guard.DebugAssertArgumentNotNull(@event, nameof(@event));
+            Guard.DebugAssertArgument(CanHandle(@event), nameof(@event));
+
+            if (@event is SensorActivatedEvent)
+            {
+                _eventPublisher.PublishEvent(new TurnOnEvent(SwitchId));
+            }
+            else if (@event is SensorDeactivatedEvent)
+            {
+                _eventPublisher.PublishEvent(new TurnOffEvent(SwitchId));
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
