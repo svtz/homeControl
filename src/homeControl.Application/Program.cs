677173ffb43d39f4e0103e728f9274c46b47fa5a
Svyatoslav Zakhryapin <svt.zhr@gmail.com>
2016-12-19 01:41:56 +0300

    WebApi listener: the beginning.

--- src/homeControl.Application/Program.cs
+++ src/homeControl.Application/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 using homeControl.Application.IoC;
 using homeControl.Configuration;
 using homeControl.Core;
+using homeControl.WebApi;
 using StructureMap;
 
 namespace homeControl.Application
@@ -14,20 +16,30 @@ namespace homeControl.Application
             var container = new Container(c => c.AddRegistry<ApplicationRegistry>());
             using (var child = container.GetNestedContainer())
             {
-                var initializers = child.GetAllInstances<IInitializer>();
-                foreach (var initializer in initializers)
-                {
-                    initializer.Init();
-                }
+                Run(child);
+            }
+        }
 
-                var loop = child.GetInstance<EventProcessingLoop>();
-                loop.ThrottleTime = TimeSpan.FromMilliseconds(100);
+        private static void Run(IContainer child)
+        {
+            var initializers = child.GetAllInstances<IInitializer>();
+            foreach (var initializer in initializers)
+            {
+                initializer.Init();
+            }
 
-                using (var cts = new CancellationTokenSource())
-                {
-                    Console.CancelKeyPress += (s, e) => cts.Cancel();
-                    loop.Run(cts.Token);
-                }
+            var loop = child.GetInstance<EventProcessingLoop>();
+            loop.ThrottleTime = TimeSpan.FromMilliseconds(100);
+
+            var entryPoint = child.GetInstance<WebApiEntryPoint>();
+
+            using (var cts = new CancellationTokenSource())
+            {
+                Console.CancelKeyPress += (s, e) => cts.Cancel();
+                var loopTask = Task.Factory.StartNew(() => loop.Run(cts.Token));
+                var apiTask = Task.Factory.StartNew(() => entryPoint.Run(cts.Token));
+
+                Task.WaitAll(loopTask, apiTask);
             }
         }
     }
---
+++ src/homeControl.WebApi/WebApiEntryPoint.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using homeControl.Core;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using StructureMap;
+using StructureMap.TypeRules;
+
+namespace homeControl.WebApi
+{
+    public sealed class WebApiEntryPoint
+    {
+        private readonly IContainer _container;
+
+        public WebApiEntryPoint(IContainer container)
+        {
+            _container = container;
+        }
+
+        public void Run(CancellationToken ct)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureServices(ForwardContainerRegistrations)
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:5000")
+                .Build();
+
+            host.Run(ct);
+        }
+
+        private Assembly[] GetForwardTypeAssemblies()
+        {
+            return new[]
+                {
+                    typeof(IEventPublisher),
+                }
+                .Select(type => type.GetAssembly())
+                .ToArray();
+        }
+
+        private void ForwardContainerRegistrations(IServiceCollection services)
+        {
+            var forwardAssemblies = GetForwardTypeAssemblies();
+            foreach (var plugin in _container.Model.PluginTypes)
+            {
+                var pluginType = plugin.PluginType;
+                var pluginAssembly = pluginType.GetAssembly();
+                if (!forwardAssemblies.Contains(pluginAssembly))
+                {
+                    continue;
+                }
+
+                var serviceDescription = ServiceDescriptor.Transient(pluginType, s => _container.GetInstance(pluginType));
+                services.Add(serviceDescription);
+            }
+        }
+    }
+}
