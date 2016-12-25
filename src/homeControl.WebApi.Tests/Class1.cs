9b80df199a2ae23cfa7beb816874d1568623bace
Svyatoslav Zakhryapin <svt.zhr@gmail.com>
2016-12-25 16:40:25 +0300

    tests project for WebApi

---
+++ src/homeControl.WebApi.Tests/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace homeControl.WebApi.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+    }
+}
