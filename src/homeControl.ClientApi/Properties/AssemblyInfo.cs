920fbadea1f6ce9189dae896a30e47d7552c4339
Svyatoslav Zakhryapin <svt.zhr@gmail.com>
2017-03-12 00:21:43 +0300

    Rename WebApi->ClientApi. Part 2.

--- src/homeControl.ClientApi/Properties/AssemblyInfo.cs
+++ src/homeControl.ClientApi/Properties/AssemblyInfo.cs
@@ -9,6 +9,6 @@ using System.Runtime.InteropServices;
 // The following Id is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8B5A1567-560D-4BFF-8649-3D2000A2B375")]
 
-[assembly: InternalsVisibleTo("homeControl.WebApi.Tests")]
+[assembly: InternalsVisibleTo("homeControl.ClientApi.Tests")]
 [assembly: InternalsVisibleTo("homeControl.Experiments")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
