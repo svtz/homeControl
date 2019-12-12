3ae51c5153b80157a3324252cc3f73ff3e9a3efc
Svyatoslav Zakhryapin <svt.zhr@gmail.com>
2019-12-12 23:44:08 +0300

    use docker network

--- src/rabbitmq/init.sh
+++ src/rabbitmq/init.sh
@@ -3,12 +3,14 @@
 ( sleep 60 ; \
 rabbitmqctl add_user controller controller 2>/dev/null ; \
 rabbitmqctl add_user noolite noolite 2>/dev/null ; \
+rabbitmqctl add_user noolitef noolitef 2>/dev/null ; \
 rabbitmqctl add_user configStore configStore 2>/dev/null ; \
 rabbitmqctl add_user client client 2>/dev/null ; \
 rabbitmqctl add_vhost homeControl ; \
 rabbitmqctl set_permissions -p homeControl configStore ".*" ".*" ".*" ; \
 rabbitmqctl set_permissions -p homeControl controller ".*" ".*" ".*" ; \
 rabbitmqctl set_permissions -p homeControl noolite ".*" ".*" ".*" ; \
+rabbitmqctl set_permissions -p homeControl noolitef ".*" ".*" ".*" ; \
 rabbitmqctl set_permissions -p homeControl client ".*" ".*" ".*" ; \
 echo "*** Users completed. ***") &
 
