bc75f5642d2e14f26c67594b381e23cfdc847132
Svyatoslav Zakhryapin <svt.zhr@gmail.com>
2020-03-28 22:45:14 +0300

    debug helpers

---
+++ src/rabbitmq/init-management.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+( sleep 60 ; \
+rabbitmqctl add_user admin admin 2>/dev/null ; \
+rabbitmqctl set_user_tags admin administrator ; \
+rabbitmqctl set_permissions -p / admin ".*" ".*" ".*" ; \
+
+rabbitmqctl add_user controller controller 2>/dev/null ; \
+rabbitmqctl add_user noolite noolite 2>/dev/null ; \
+rabbitmqctl add_user noolitef noolitef 2>/dev/null ; \
+rabbitmqctl add_user configStore configStore 2>/dev/null ; \
+rabbitmqctl add_user client client 2>/dev/null ; \
+rabbitmqctl add_vhost homeControl ; \
+rabbitmqctl set_permissions -p homeControl configStore ".*" ".*" ".*" ; \
+rabbitmqctl set_permissions -p homeControl controller ".*" ".*" ".*" ; \
+rabbitmqctl set_permissions -p homeControl noolite ".*" ".*" ".*" ; \
+rabbitmqctl set_permissions -p homeControl noolitef ".*" ".*" ".*" ; \
+rabbitmqctl set_permissions -p homeControl client ".*" ".*" ".*" ; \
+echo "*** Users completed. ***") &
+
+# $@ is used to pass arguments to the rabbitmq-server command.
+# For example if you use it like this: docker run -d rabbitmq arg1 arg2,
+# it will be as you run in the container rabbitmq-server arg1 arg2
+rabbitmq-server $@
